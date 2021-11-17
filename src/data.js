4112bc0c948edb6da921be5c0591b6f591b24e1f
Henrique Torres Rosa <zique.torres@gmail.com>
2021-11-17 16:05:27 -0300

    'mais ajustes de posicionamento'

--- src/data.js
+++ src/data.js
@@ -2,40 +2,39 @@ export const projetosDHPortfolio = [
     {
         id: 1,
         title: "En Garde!",
-        img:
-        "https://media.discordapp.net/attachments/804525026706718754/910364824687489034/unknown.png?width=768&height=578",
+        img: "../../assets/En Garde!.png",
+        src: "https://zique02.github.io/projeto01/Desafio-01/",
     },
     {
         id: 2,
-        title: "En Garde!",
-        img:
-        "https://media.discordapp.net/attachments/804525026706718754/910364824687489034/unknown.png?width=768&height=578",
+        title: "DOPE SKETCH",
+        img: "../../assets/Screenshot_2.png",
+        src: "https://zique02.github.io/projeto01/Desafio-02/",
     },
     {
         id: 3,
-        title: "En Garde!",
-        img:
-        "https://media.discordapp.net/attachments/804525026706718754/910364824687489034/unknown.png?width=768&height=578",
+        title: "Infinity",
+        img: "../../assets/infinity.png",
+        src: "https://zique02.github.io/FrontEnd_Empresa/home/index.html",
     },
 ];
 
 export const projetosPessoaisPortfolio = [
     {
         id: 1,
-        title: "En Garde!",
-        img:
-        "https://media.discordapp.net/attachments/804525026706718754/910364824687489034/unknown.png?width=768&height=578"
+        title: "Primeiro site",
+        img: "../../assets/Primeiro-site.png",
+        src: "https://zique02.github.io/Front-End/aula7/",
     },
     {
         id: 2,
         title: "En Garde!",
-        img:
-        "https://media.discordapp.net/attachments/804525026706718754/910364824687489034/unknown.png?width=768&height=578"
+        img: "https://media.discordapp.net/attachments/804525026706718754/910364824687489034/unknown.png?width=768&height=578",
     },
     {
         id: 3,
         title: "En Garde!",
-        img:
-        "https://media.discordapp.net/attachments/804525026706718754/910364824687489034/unknown.png?width=768&height=578"
+        img: "https://media.discordapp.net/attachments/804525026706718754/910364824687489034/unknown.png?width=768&height=578",
+        src: "",
     },
 ]
